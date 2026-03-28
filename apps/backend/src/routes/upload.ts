72bcb048a563601234cada94d6519bafebd84c02
Icaro Fernandes <icaroomanuel@gmail.com>
2026-03-28 13:26:48 -0300

    add: added cloudflare config and route

---
+++ apps/backend/src/routes/upload.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from 'fastify';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { r2 } from '../lib/cloudflare';
+import crypto from 'node:crypto';
+import { extname } from 'node:path';
+import { verifyJwt } from '../midlewares/verify-jwt';
+
+export async function uploadRoutes(app: FastifyInstance) {
+app.addHook('preHandler', verifyJwt);
+
+app.post('/', async (request, reply) => {
+  const upload = await request.file();
+  if (!upload) {
+        return reply.status(400).send({ message: 'Nenhum arquivo enviado.' });
+      }
+
+  const mimeTypeRegex = /^(image)\/[a-zA-Z]+/;
+  const isValidFileFormat = mimeTypeRegex.test(upload.mimetype);
+  if (!isValidFileFormat) {
+    return reply.status(400).send({ message: 'Formato de arquivo inválido.' });
+  }
+  const fileId = crypto.randomUUID();
+      const extension = extname(upload.filename);
+      const fileName = `${fileId}${extension}`;
+
+  const buffer = await upload.toBuffer();
+  const command = new PutObjectCommand({
+        Bucket: process.env.CLOUDFLARE_BUCKET_NAME,
+        Key: fileName,
+        Body: buffer,
+        ContentType: upload.mimetype,
+      });
+  await r2.send(command);
+  const fileUrl = `${process.env.CLOUDFLARE_PUBLIC_URL}/${fileName}`;
+
+  return reply.status(201).send({ posterUrl: fileUrl });
+})
+
+}
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,6 +3,8 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import { authRoutes } from './routes/auth';
 import fastifyJwt from '@fastify/jwt';
 import { moviesRoutes } from './routes/movies';
+import fastifyMultipart from '@fastify/multipart';
+import { uploadRoutes } from './routes/upload';
 
 const app = fastify();
 
@@ -12,9 +14,12 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(fastifyJwt, {
   secret: process.env.JWT_SECRET as string,
 })
+app.register(fastifyMultipart);
 
 app.register(authRoutes, {prefix: '/auth'});
 app.register(moviesRoutes, { prefix: '/movies' })
+app.register(uploadRoutes, { prefix: '/upload' })
+
 
 app.listen({port: 3333, host: '0.0.0.0'}).then(() => {
   console.log('🚀 Servidor HTTP rodando na porta 3333!');
